e46f63c1003b4e56f79dc607b1ad786cfa1e1f5d
Gaurav Ghodinde <65962770+gauravghodinde@users.noreply.github.com>
2023-10-08 20:50:19 +0530

    fixed the path of the local image logo.png

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "744297c683fc17cf5ec5d0bc5c3dc27b",
-"/": "744297c683fc17cf5ec5d0bc5c3dc27b",
-"main.dart.js": "edf76fe600da774ec877d4fcdc5fb9ba",
+"index.html": "4815ebfc60492b59a0bac288a7ada034",
+"/": "4815ebfc60492b59a0bac288a7ada034",
+"main.dart.js": "6af4d4ca170c722232e595aaa015091c",
 "manifest.json": "0ec638e0979f0db4db973027ab22cdf7",
 "version.json": "3a3952c38dc36115f76066cc6a5bd3eb"
 };
